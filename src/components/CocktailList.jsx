40a9aafcbf8a3dee33d45df0b957fc24fb12995d
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-08-15 19:57:24 +0530

    Added Cocktail List component

---
+++ src/components/CocktailList.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCocktails } from "../redux/features/cocktailSlice";
+import { Link } from "react-router-dom";
+
+const CocktailList = () => {
+  return <div>CocktailList</div>;
+};
+
+export default CocktailList;
